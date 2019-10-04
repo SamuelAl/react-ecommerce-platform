2b3c4091c860654543344648ed4b30f3ad540edb
einstenz <33717014+einstenz@users.noreply.github.com>
2019-10-04 18:23:12 +0100

    Updated homepage component with custom SASS

--- sammy-clothing/src/App.js
+++ sammy-clothing/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
 import './App.css';
+
+import HomePage from "./components/HomePage/homepage.component";
 
 function App() {
   return (
     <div>
-      Hello World
+      <HomePage />
     </div>
   );
 }
