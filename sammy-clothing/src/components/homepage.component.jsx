96a6c3fca95d5bf1d50aa6ddb9307f9fe9b3e644
einstenz <33717014+einstenz@users.noreply.github.com>
2019-10-04 17:28:38 +0100

    Create homepage.component.jsx

---
+++ sammy-clothing/src/components/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => (
+  <div className = "homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+        </div>
+      </div>
+    </div>
+  </div>
+)
